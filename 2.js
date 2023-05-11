30d011d50d32871be0fa84bb9b4127dca3adb7db
Ayush Chandra <ace9009@outlook.com>
2023-05-11 17:37:26 +0530

    new one

--- 2.js
+++ 2.js
@@ -1,7 +1,8 @@
 function MApp() {
   const [chatVisible, setChatVisible] = useState(false);
-const messageicon = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Un1.svg/1200px-Un1.svg.png"
-const close = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Un1.svg/1200px-Un1.svg.png"
+  const messageicon = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Un1.svg/1200px-Un1.svg.png"
+  const close = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Un1.svg/1200px-Un1.svg.png"
+
   const handleImageClick = () => {
     setChatVisible(!chatVisible);
     console.log(chatVisible);
@@ -53,32 +54,25 @@ const close = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Un1.svg
         "Thanks for letting your Angular issue, Our team will resolve your issue ASAP",
     },
   ];
-  return (
-    "<div>" +
-    (!chatVisible ? (
-      "<img" +
-      ' style="position: fixed; bottom: 20px; right: 20px; cursor: pointer;"' +
-      ' src={messageicon}' +
-      ' alt="Chat Icon"' +
-      ' onClick={handleImageClick}' +
-      "/>"
-    ) : (
-      '<div style="position: fixed; bottom: 20px; right: 20px;">' +
-      '<img' +
-      ' src={close}' +
-      ' alt="Close Icon"' +
-      ' style="position: absolute; top: -504px; right: 65px; cursor: pointer; height: 30px; z-index: 1;"' +
-      ' onClick={handleImageClick}' +
-      "/>" +
-      '<div style="position: fixed; bottom: 20px; right: 20px;">' +
-      '<Segment style="width: 400px;">' +
-      "<ChatBot steps={steps} />" +
-      "</Segment>" +
-      "</div>" +
-      "</div>"
-    )) +
-    "</div>"
-  );
+
+  let output = '';
+
+  if (!chatVisible) {
+    output += `<img style="position: fixed; bottom: 20px; right: 20px; cursor: pointer;" src={messageicon} alt="Chat Icon" onClick={handleImageClick} />`;
+  } else {
+    output += `
+      <div style="position: fixed; bottom: 20px; right: 20px;">
+        <img src={close} alt="Close Icon" style="position: absolute; top: -504px; right: 65px; cursor: pointer; height: 30px; z-index: 1;" onClick={handleImageClick} />
+        <div style="position: fixed; bottom: 20px; right: 20px;">
+          <div style="width: 400px;">
+            <ChatBot steps={steps} />
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  return `<div>${output}</div>`;
 }
 
 
